51866d6a12b1b5238b4f4e19f7c2c01cc740bc9e
kandagar1988 <vitalijtyckovskij@gmail.com>
2026-02-08 00:53:44 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -65,3 +65,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("resize", updateSlider);
 });
+// mobile menu
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.main-nav');
+
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
